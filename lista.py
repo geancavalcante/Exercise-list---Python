3ebe88a1677a857dc552642ee21d9ddd582bd9c8
Gean <gean.dev2018@gmal.com>
2024-08-22 12:34:18 -0300

    first commit

---
+++ lista.py
@@ -0,0 +1,29 @@
+
+class Lista():
+    def __init__(self):
+        self.lista = []
+
+
+    def append_in_list(self):
+        exercise  = str(input('Digite a matéria e a Página:'))
+        self.lista.append(exercise)
+        print('')
+        print(f"'{exercise}', adiconada com sucesso!")
+
+
+    def remove_in_list(self):
+            exercise_remove = input('Qual tarefa quer remover?')
+            try:
+                self.lista.remove(exercise_remove)
+                print(f"'{exercise_remove}', Exercício removido com suscesso!")
+            except:
+                print('')
+                print(f"Não possue o item '{exercise_remove}' na lista")
+                
+    def see_list(self):
+        return self.lista
+    
+    def size_list(self):
+        return len(self.lista)
+    
+
---
+++ usuario.py
@@ -0,0 +1,28 @@
+from lista import Lista
+
+lista = Lista()
+
+while True:
+    print('')
+    print('='*60)
+    print("1.Adicionar Exercício","2.Remover Exercício", "3.Ver Lista", "4.Ver número de Exercício", sep=' | ')
+    print()
+    exercise  = int(input('Oque deseja fazer?'))
+    if exercise == 1:
+        print('')
+        lista.append_in_list()
+    elif exercise == 2:
+        print('')
+        lista.remove_in_list()
+    elif exercise == 3:
+        print('')
+        print("Sua lista de Exercício:",lista.see_list())
+    elif exercise == 4 :
+        print('')
+        numero_itens_tarefas = lista.size_list()
+        print(f"Sua lista possue: {numero_itens_tarefas} Exercício")
+    else:
+        print('Tente Novamente!')
+
+
+
